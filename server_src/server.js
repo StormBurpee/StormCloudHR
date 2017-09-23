b503dbc99628b5a0e422532ec5c547796fa32c4c
Storm Burpee <Storm@storms-mbp.lan>
2017-09-23 18:15:32 +0930

    mysql database

--- server_src/server.js
+++ server_src/server.js
@@ -1,14 +1,30 @@
-var express = require('express');
+var express   = require('express');
+var redis     = require("redis");
+var bluebird  = require('bluebird');
+var mysql     = require('mysql');
+
 var app = express();
 var port = process.env.PORT || 3000;
-var redis = require("redis");
-var bluebird = require('bluebird');
+
+var db = mysql.createConnection({
+  host: "104.197.10.100",
+  user: "stormcloudhr",
+  password: "stormcloud123",
+  database: "stormcloudhr_default"
+});
+
+db.connect((err) => {
+  if(err)
+    throw err;
+  console.log("Connected to MySQL Database.");
+});
 
 var redisConnection = {ip: "127.0.0.1", port: "6379"};
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 var rclient = redis.createClient(redisConnection.port, redisConnection.ip);
+console.log("Connected to Redis Cache/Database.");
 
 rclient.hmset('StormCellHR', {
   "version": "0.0.1",
@@ -25,3 +41,4 @@ router.get('/', function(request, response) {
 
 app.use('/', router);
 app.listen(port);
+console.log("Server Initalized and Online.");
