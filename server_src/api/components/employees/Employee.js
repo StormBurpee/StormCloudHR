409a78c84a22cb16b0302657de3d4b38ecd1e30e
Storm Burpee <Storm@storms-mbp.lan>
2017-09-24 08:35:16 +0930

    debugging

--- server_src/api/components/employees/Employee.js
+++ server_src/api/components/employees/Employee.js
@@ -12,7 +12,8 @@ class Employee extends Model {
       rclient.hgetall('stormcellhr_employees_'+company, function(err, object) {
         if(object != null && object.employees != null) {
           console.log("Received Employees from Redis Cache");
-          resolve(JSON.stringify(object.employees));
+          console.log(object.employees);
+          resolve(object.employees);
         } else {
           db.query("SELECT * FROM employees WHERE belongs_to="+company, (err, rows) => {
             console.log("Sending Request for employees to DB.");
