c449f3d84d7a97807713da7732044d4f47594116
Storm Burpee <Storm@storms-mbp.lan>
2017-09-24 08:18:10 +0930

    Debugging

--- server_src/api/components/employees/Employee.js
+++ server_src/api/components/employees/Employee.js
@@ -6,6 +6,7 @@ class Employee extends Model {
   }
 
   getEmployees(company, db) {
+    console.log(company, db);
     return new Promise((resolve, reject) => {
       this.rclient.hgetall('stormcellhr_employees_'+company, function(err, object) {
         if(object != null && object.employees != null) {
