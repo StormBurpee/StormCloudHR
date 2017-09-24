a26b543f6e2f6fe61b3a1dd73562111773ad7f17
Greyshaun33 <greyshaun33@gmail.com>
2017-09-24 20:15:55 +1000

    started

---
+++ src/components/EmployeeNew/HireStepper.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'hire-stepper'
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+</style>
