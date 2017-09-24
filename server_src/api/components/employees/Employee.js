d2ed96f6b26d860173e5cddb1511506c8ff10402
Greyshaun33 <greyshaun33@gmail.com>
2017-09-24 11:43:59 +1000

    forgot to reference variable

--- server_src/api/components/employees/Employee.js
+++ server_src/api/components/employees/Employee.js
@@ -83,7 +83,7 @@ class Employee extends Model {
       rclient.hgetall('stormcellhr_employees_'+company, function(err, object) {
         if(object != null && object.employees != null) {
           console.log("Received Employees from Redis Cache");
-          resolve(JSON.parse(object));
+          resolve(JSON.parse(object.employees));
         } else {
           db.query("SELECT * FROM employees WHERE belongs_to="+company, (err, rows) => {
             console.log("Sending Request for employees to DB.");
